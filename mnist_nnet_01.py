3bd04ea8e50d943f7f0e73765486fbb0e797f9b5
danpilgrim <d.daniel.pilgrim@gmail.com>
2018-05-12 23:07:20 -0700

    mnist

---
+++ mnist_nnet_01.py
@@ -0,0 +1,9 @@
+
+
+# weighted sum of inputs plus a constant fed through an activation function
+# different types of activation functions
+
+# Ex: SOFTMAX(Ln)= e^L/||e^L||
+# Y = SOFTMAX(X*W + b)
+# Predictions = softmax(Images X[100,784] * weights W[784,10] + (each line) Biases[10])
+#
